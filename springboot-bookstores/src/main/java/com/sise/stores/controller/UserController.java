23ceb921b5ee730a3a4332e6a60f5352cbf95994
littlefiy <1428905003@qq.com>
2021-06-07 16:09:06 +0800

    project commit

---
+++ springboot-bookstores/src/main/java/com/sise/stores/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.sise.stores.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/test")
+    public String test(){
+        return "hello";
+    }
+}
